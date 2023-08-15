f3eddd463d35b48ed4b798c9e4320018c21b7d80
KAnanev <russrage@yandex.ru>
2023-08-15 17:19:27 +0300

    добавил index

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
     path('admin/', admin.site.urls),
 ]
