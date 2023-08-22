cad106cb8030a16845d8ed1c2d995548030e039b
KAnanev <russrage@yandex.ru>
2023-08-22 21:57:29 +0300

    вернул миграцию

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+
+# Create superuser
+echo "Migrate "
+make migrate
+
 # Create superuser
 echo "Create superuser"
-poetry run python manage.py create_superuser
+make create_superuser
 
 # Collect staticfiles
 echo "Collect staticfiles"
-poetry run python manage.py collectstatic --no-input
+make collect_static
 
 # Start server
 echo "Starting server"
