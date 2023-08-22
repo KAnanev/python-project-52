07f05dcc1e3a28101de7c17cf18af7bef6b626cf
KAnanev <ananevdeveloper@yandex.ru>
2023-08-22 19:59:28 +0300

    Корректировка

--- task_manager/settings/components/base.py
+++ task_manager/settings/components/base.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', False) == 'True'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
