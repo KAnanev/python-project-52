65bbd65e2dec3d79e6eb44f21eb48ee62ef415d3
KAnanev <russrage@yandex.ru>
2023-08-22 21:33:01 +0300

    исправил родительский каталог

--- task_manager/settings/components/base.py
+++ task_manager/settings/components/base.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -30,7 +30,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
-
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
