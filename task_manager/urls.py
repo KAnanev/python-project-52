5fabe46524b65f4949712bf36ef40d52b84e3f25
KAnanev <russrage@yandex.ru>
2023-08-23 21:09:27 +0300

    добавил логин url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base.html')),
+    path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
