10fe106bf548e63a9e269ec94668634f05118efd
KAnanev <russrage@yandex.ru>
2023-09-25 17:26:11 +0300

    Переопередлил шаблон

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
