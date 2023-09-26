5027bbb6bcd153d866e991f0220c67dbbfe5a413
KAnanev <russrage@yandex.ru>
2023-09-26 11:30:21 +0300

    Изменил путь к шаблонам

--- task_manager/settings/components/base.py
+++ task_manager/settings/components/base.py
@@ -31,12 +31,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'task_manager.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': ["task_manager/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
