d9dd41e819cf85684c64945c1dac7d878bbd049f
KAnanev <russrage@yandex.ru>
2023-09-26 12:27:48 +0300

    Добавил форму регистрации

--- task_manager/account/urls.py
+++ task_manager/account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from task_manager.account.views import UserListView
+from task_manager.account.views import UserListView, UserCreateView
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='user_list'),
-    # path('create/'),
+    path('', UserListView.as_view(), name='users'),
+    path('create/', UserCreateView.as_view(), name='signup'),
     # path('<int:pk>/update/'),
     # path('<int:pk>/delete/'),
 ]
--- task_manager/account/views.py
+++ task_manager/account/views.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import User
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+
+from task_manager.account.forms import CreateUserForm
 
 
 class UserListView(ListView):
     model = User
     template_name = 'users.html'
+
+
+class UserCreateView(SuccessMessageMixin, CreateView):
+    model = User
+    form_class = CreateUserForm
+    success_message = 'Пользователь успешно зарегистрирован'
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+    extra_context = {
+        'button_text': 'Зарегистрировать',
+    }
