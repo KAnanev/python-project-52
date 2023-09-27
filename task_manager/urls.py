575085393426575196b576004018b1f625cec851
KAnanev <russrage@yandex.ru>
2023-09-27 22:05:05 +0300

    fix lint

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView
 
 from task_manager.views import IndexView, UserLoginView, UserLogoutView
 
