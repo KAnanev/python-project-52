4212e9349ef72f509eebbb621ca1a76f054efd51
KAnanev <russrage@yandex.ru>
2023-09-28 15:46:41 +0300

    fix lint

--- task_manager/account/urls.py
+++ task_manager/account/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from task_manager.account.views import UserListView, UserCreateView, UserUpdateView
+from task_manager.account.views import (
+    UserListView,
+    UserCreateView,
+    UserUpdateView
+)
 
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
