86a5d689461af5cb9301f1fd1c5bc2bf5a0667ea
KAnanev <russrage@yandex.ru>
2023-09-28 15:45:13 +0300

    Обновление пользователя

--- task_manager/account/urls.py
+++ task_manager/account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from task_manager.account.views import UserListView, UserCreateView
+from task_manager.account.views import UserListView, UserCreateView, UserUpdateView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
     path('create/', UserCreateView.as_view(), name='signup'),
-    # path('<int:pk>/update/'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='update_user'),
     # path('<int:pk>/delete/'),
 ]
--- task_manager/account/views.py
+++ task_manager/account/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.translation import gettext as _
 
 from task_manager.account.forms import UserForm
@@ -22,4 +23,19 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     extra_context = {
         'title': _('Регистрация'),
         'button_text': _('Зарегистрировать'),
+    }
+
+
+class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    success_message = _('Пользователь успешно изменен')
+    login_url = 'login'
+    redirect_field_name = 'login'
+    success_url = reverse_lazy('login')
+    template_name = 'form.html'
+
+    extra_context = {
+        'title': _('Изменение пользователя'),
+        'button_text': _('Изменить'),
     }
