908aa95f73eb0ba2f56f7972f876df177f9332f0
KAnanev <russrage@yandex.ru>
2023-10-02 18:39:11 +0300

    Переименовал title

--- task_manager/views.py
+++ task_manager/views.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    extra_context = {'title': _('Менеджер задач')}
+    extra_context = {'title': _('Менеджер задач Hexlet')}
 
 
 class UserLoginView(SuccessMessageMixin, LoginView):
