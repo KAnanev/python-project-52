cd15f00bb5ef9d600c24495a70baf7a7eb81c076
KAnanev <russrage@yandex.ru>
2023-10-04 07:15:38 +0300

    Админ панель для users

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(_('Имя'), max_length=150)
+    last_name = models.CharField(_('Фамилия'), max_length=150)
+
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
 
     def __str__(self):
         return self.get_full_name()
