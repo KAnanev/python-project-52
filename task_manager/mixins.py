7d1e151b5a61211bb015203ed97bd0d275d05d70
KAnanev <russrage@yandex.ru>
2023-10-05 21:53:05 +0300

    Переписал доступы, как миксины

---
+++ task_manager/mixins.py
@@ -0,0 +1,29 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
+
+
+class AuthRequiredMixin(LoginRequiredMixin):
+
+    auth_message = _('Вы не авторизованы! Пожалуйста, выполните вход.')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, self.auth_message)
+            return redirect(reverse_lazy('login'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class UserPermissionMixin(UserPassesTestMixin):
+
+    permission_message = None
+    permission_url = None
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.permission_message)
+        return redirect(self.permission_url)
