e27f9a53252972c8f2d2a67c55cfa618e6c4f310
KAnanev <russrage@yandex.ru>
2023-10-05 18:05:36 +0300

    Представление для TaskStatus

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from task_manager.statuses.models import TaskStatus
+
+
+class StatusesView(ListView):
+    model = TaskStatus
+    template_name = 'statuses.html'
