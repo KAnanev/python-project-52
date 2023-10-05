8d2cb1a62ffe27c48aee5b3bbf6f5b945e909c71
KAnanev <russrage@yandex.ru>
2023-10-05 17:57:31 +0300

    Модель для TaskStatus

---
+++ task_manager/statuses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(
+        max_length=50,
+        blank=False,
+        unique=True,
+        verbose_name=_('Название'),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Дата создания')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Статус')
+        verbose_name_plural = _('Статусы')
