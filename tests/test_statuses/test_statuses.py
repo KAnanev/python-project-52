561b923880b0d668385d9be2e0e02ffea1e57b12
KAnanev <russrage@yandex.ru>
2023-10-06 11:32:34 +0300

    Тесты создания статусов

--- tests/test_statuses/test_statuses.py
+++ tests/test_statuses/test_statuses.py
@@ -2,6 +2,9 @@ import pytest
 from django.urls import reverse
 
 URL = reverse('statuses')
+CREATE_STATUSES_URL = reverse('create_status')
+
+STATUS = 'В работе'
 
 
 def test_without_login_statuses_view(client):
@@ -22,3 +25,21 @@ def test_statuses_view(client_with_login_test_user_1, client):
 
     assert response.context['title'] == 'Статусы'
     assert '<title>Статусы</title>' in response.content.decode('utf8')
+
+
+def test_get_create_statuses_view(client):
+
+    response = client.get(URL)
+    assert response.status_code == 200
+    assert response.context['title'] == 'Создать статус'
+    assert '<title>Создать статус</title>' in response.content.decode('utf8')
+
+
+def test_post_create_statuses_view(client):
+
+    response = client.post(URL, {'name': STATUS}, follow=True)
+    message = list(response.context.get('messages'))[0]
+
+    assert response.status_code == 200
+    assert STATUS in response.content.decode('utf8')
+    assert 'Статус успешно создан' in message.message
