6be7f46e719d147c908de02aeac56c85bf7f6557
KAnanev <russrage@yandex.ru>
2023-10-08 19:41:50 +0300

    Тест представления задач

---
+++ tests/test_tasks/test_tasks.py
@@ -0,0 +1,21 @@
+from django.urls import reverse
+
+
+class TestViewTasks:
+
+    url = reverse('tasks')
+    title = 'Изменение пользователя'
+
+    def test_view_tasks_without_login(self, client):
+        response = client.get(self.url)
+        assert response.status_code == 302
+        assert response['Location'] == reverse('login')
+
+    def test_view_tasks_with_login(self, client_with_login_test_user_1, client):
+        response = client.get(self.url)
+        assert response.status_code == 200
+        assert response['Location'] == reverse('tasks')
+        assert response.context['title'] == self.title
+        assert f'<title>{self.title}</title>' in response.content.decode('utf8')
+
+
