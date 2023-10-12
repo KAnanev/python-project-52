5b15dc5c5f1dab68cfb3c5a26c8dfe500f471669
KAnanev <russrage@yandex.ru>
2023-10-12 16:55:58 +0300

    оптимизировал тесты для задач

--- tests/test_tasks/test_tasks.py
+++ tests/test_tasks/test_tasks.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 
 
@@ -5,13 +6,16 @@ class BaseTask:
     url = None
     title = None
 
-    def test_view_tasks_without_login(self, client):
-        response = client.get(self.url)
+    @pytest.fixture
+    def response(self, client):
+        return client.get(self.url)
+
+    @staticmethod
+    def test_view_tasks_without_login(response):
         assert response.status_code == 302
         assert response['Location'] == reverse('login')
 
-    def test_view_tasks_with_login(self, client_with_login_test_user_1, client):
-        response = client.get(self.url)
+    def test_view_tasks_with_login(self, client_with_login_test_user_1, response):
         assert response.status_code == 200
         assert response.context['title'] == self.title
         assert f'<title>{self.title}</title>' in response.content.decode('utf8')
@@ -26,8 +30,8 @@ class TestCreateTask(BaseTask):
     url = reverse('create_tasks')
     title = 'Создать задачу'
 
-    def test_create_task_without_login(self, client):
-        super().test_view_tasks_without_login(client)
+    def test_create_task_without_login(self, response):
+        super().test_view_tasks_without_login(response)
 
-    def test_create_task_with_login(self, client_with_login_test_user_1, client):
-        super().test_view_tasks_with_login(client_with_login_test_user_1, client)
+    def test_create_task_with_login(self, client_with_login_test_user_1, response):
+        super().test_view_tasks_with_login(client_with_login_test_user_1, response)
