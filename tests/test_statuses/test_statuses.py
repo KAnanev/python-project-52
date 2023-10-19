ea8496935bf235061b2acfe58c626c06a2929a7e
KAnanev <russrage@yandex.ru>
2023-10-19 21:05:53 +0300

    Рефактор тестов

--- tests/test_statuses/test_statuses.py
+++ tests/test_statuses/test_statuses.py
@@ -38,7 +38,7 @@ class TestCreateStatus(BaseTest):
         assert '<title>Создать статус</title>' in response.content.decode('utf8')
 
     def test_post_create_status_view(self, login_user_a, client_post):
-        """Создание статуса."""
+        """Тест создания статуса."""
         status_name = 'В работе'
 
         response = client_post(data={'name': status_name}, follow=True)
@@ -63,7 +63,7 @@ class TestUpdateStatus(BaseTest):
         assert '<title>Изменение статуса</title>' in response.content.decode('utf8')
 
     def test_post_update_status_view(self, status_in_db, login_user_a, client_post):
-        """Обновление статуса."""
+        """Тест обновления статуса."""
 
         update_status_name = 'Выполнено'
         response = client_post(pk=status_in_db.pk, data={'name': update_status_name}, follow=True)
@@ -75,3 +75,29 @@ class TestUpdateStatus(BaseTest):
         assert status.name == update_status_name
         assert status.name in response.content.decode('utf8')
         assert 'Статус успешно изменен' in message.message
+
+
+class TestDeleteStatus(BaseTest):
+    view_name = 'delete_status'
+
+    def test_get_delete_status(self, status_in_db, login_user_a, client_get):
+        """Тест страницы удаления статуса."""
+
+        response = client_get(pk=status_in_db.pk)
+
+        assert response.status_code == 200
+        assert response.context['title'] == 'Удаление статуса'
+        assert 'Да, удалить' in response.content.decode('utf8')
+        assert f'Вы уверены, что хотите удалить {status_in_db.name}?' in response.content.decode('utf8')
+        assert 'class="btn btn-danger"' in response.content.decode('utf8')
+
+    def test_post_delete_status(self, status_in_db, login_user_a, client_post):
+        """Тест удаления статуса."""
+
+        response = client_post(pk=status_in_db.pk, follow=True)
+        statuses = TaskStatus.objects.all()
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert not len(statuses)
+        assert 'Статус успешно удален' in message.message
