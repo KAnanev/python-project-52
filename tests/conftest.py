105083eb5a52262b5b82dd41bfc0cc7c107eb950
KAnanev <russrage@yandex.ru>
2023-10-20 20:56:50 +0300

    Рефактор тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 
 from task_manager.statuses.models import TaskStatus
+from task_manager.tasks.models import Task
 
 
 @pytest.fixture
@@ -30,6 +31,7 @@ def user_b():
 def create_user(db, django_user_model):
     def make_user(**kwargs):
         return django_user_model.objects.create_user(**kwargs)
+
     return make_user
 
 
@@ -54,12 +56,35 @@ def login_user_b(client, create_user_b):
 
 
 @pytest.fixture
-def status_in_db(db):
-    return TaskStatus.objects.create(name='В работе')
+def status_data():
+    return {
+        'name': 'В работе',
+    }
+
+
+@pytest.fixture
+def create_status(db, status_data):
+    status = TaskStatus.objects.create(name=status_data['name'])
+    return status
+
+
+@pytest.fixture
+def task_data(create_user_a, create_status):
+    return {
+        'name': 'Написать письмо',
+        'description': 'Написать содержательное письмо',
+        'status': create_status.pk,
+        'executor': create_user_a.pk,
+    }
+
+
+@pytest.fixture
+def create_task(db, task_data):
+    task = Task.objects.create(**task_data)
+    return task
 
 
 class BaseTest:
-
     view_name = None
 
     def get_url(self, *args, **kwargs):
@@ -80,4 +105,57 @@ class BaseTest:
             if kwargs.get('pk'):
                 return client.post(self.get_url(kwargs.pop('pk')), **kwargs)
             return client.post(self.get_url(), **kwargs)
+
         return inner
+
+
+class BaseViewTest:
+    title = None
+    button_text = None
+    error_message = None
+
+    def test_without_login_view(self, client_get):
+        """Не авторизованный пользователь, не видит страницу."""
+
+        response = client_get(follow=True)
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert self.error_message in message.message
+
+    def test_view(self, login_user_a, client_get):
+        """Авторизованный пользователь видит страницу."""
+
+        response = client_get()
+
+        assert response.status_code == 200
+        assert response.context['title'] == self.title
+        assert f'<title>{self.title}</title>' in response.content.decode('utf8')
+        assert self.button_text in response.content.decode('utf8')
+
+
+class BaseCreateTest:
+    view_name = None
+    title = None
+    button_text = None
+    post_data = None
+    create_success_message = None
+
+    def test_get_create_view(self, login_user_a, client_get):
+        """Тест страницы создания."""
+        response = client_get()
+
+        assert response.status_code == 200
+        assert response.context['title'] == self.title
+        assert f'<title>{self.title}</title>' in response.content.decode('utf8')
+
+    def test_post_create_view(self, login_user_a, client_post):
+        """Тест создания."""
+
+        response = client_post(data=self.post_data, follow=True)
+        message = list(response.context.get('messages'))[0]
+        status = TaskStatus.objects.first()
+
+        assert response.status_code == 200
+        assert 'Статус успешно создан' in message.message
+        assert status.name == self.post_data.name
--- tests/test_statuses/test_statuses.py
+++ tests/test_statuses/test_statuses.py
@@ -1,29 +1,14 @@
 from django.urls import reverse
 
 from task_manager.statuses.models import TaskStatus
-from tests.conftest import BaseTest
+from tests.conftest import BaseTest, BaseViewTest
 
 
-class TestStatusesView(BaseTest):
+class TestStatusesView(BaseViewTest, BaseTest):
     view_name = 'statuses'
-
-    def test_without_login_statuses_view(self, client_get):
-        """Не авторизованный пользователь, не видит страницу статусов."""
-
-        response = client_get(follow=True)
-        message = list(response.context.get('messages'))[0]
-
-        assert response.status_code == 200
-        assert 'Вы не авторизованы! Пожалуйста, выполните вход.' in message.message
-
-    def test_status_view(self, login_user_a, client_get):
-        """Авторизованный пользователь видит страницу статусов."""
-
-        response = client_get()
-
-        assert response.context['title'] == 'Статусы'
-        assert '<title>Статусы</title>' in response.content.decode('utf8')
-        assert 'Создать статус' in response.content.decode('utf8')
+    title = 'Статусы'
+    button_text = 'Создать статус'
+    message = 'Вы не авторизованы! Пожалуйста, выполните вход.'
 
 
 class TestCreateStatus(BaseTest):
@@ -53,22 +38,22 @@ class TestCreateStatus(BaseTest):
 class TestUpdateStatus(BaseTest):
     view_name = 'update_status'
 
-    def test_get_update_status_view(self, status_in_db, login_user_a, client_get):
+    def test_get_update_status_view(self, create_status, login_user_a, client_get):
         """Тест страницы обновления статуса."""
 
-        response = client_get(pk=status_in_db.pk)
+        response = client_get(pk=create_status.pk)
 
         assert response.status_code == 200
         assert response.context['title'] == 'Изменение статуса'
         assert '<title>Изменение статуса</title>' in response.content.decode('utf8')
 
-    def test_post_update_status_view(self, status_in_db, login_user_a, client_post):
+    def test_post_update_status_view(self, create_status, login_user_a, client_post):
         """Тест обновления статуса."""
 
         update_status_name = 'Выполнено'
-        response = client_post(pk=status_in_db.pk, data={'name': update_status_name}, follow=True)
+        response = client_post(pk=create_status.pk, data={'name': update_status_name}, follow=True)
 
-        status = TaskStatus.objects.get(pk=status_in_db.pk)
+        status = TaskStatus.objects.get(pk=create_status.pk)
         message = list(response.context.get('messages'))[0]
 
         assert response.status_code == 200
@@ -80,21 +65,21 @@ class TestUpdateStatus(BaseTest):
 class TestDeleteStatus(BaseTest):
     view_name = 'delete_status'
 
-    def test_get_delete_status(self, status_in_db, login_user_a, client_get):
+    def test_get_delete_status(self, create_status, login_user_a, client_get):
         """Тест страницы удаления статуса."""
 
-        response = client_get(pk=status_in_db.pk)
+        response = client_get(pk=create_status.pk)
 
         assert response.status_code == 200
         assert response.context['title'] == 'Удаление статуса'
         assert 'Да, удалить' in response.content.decode('utf8')
-        assert f'Вы уверены, что хотите удалить {status_in_db.name}?' in response.content.decode('utf8')
+        assert f'Вы уверены, что хотите удалить {create_status.name}?' in response.content.decode('utf8')
         assert 'class="btn btn-danger"' in response.content.decode('utf8')
 
-    def test_post_delete_status(self, status_in_db, login_user_a, client_post):
+    def test_post_delete_status(self, create_status, login_user_a, client_post):
         """Тест удаления статуса."""
 
-        response = client_post(pk=status_in_db.pk, follow=True)
+        response = client_post(pk=create_status.pk, follow=True)
         statuses = TaskStatus.objects.all()
         message = list(response.context.get('messages'))[0]
 
