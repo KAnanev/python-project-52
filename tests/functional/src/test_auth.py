9de56025549bb3c5308d1f62688e7a132432f270
KAnanev <russrage@yandex.ru>
2023-10-26 18:57:03 +0300

    Рефактор авторизации

--- tests/functional/src/test_auth.py
+++ tests/functional/src/test_auth.py
@@ -1,55 +1,55 @@
 import pytest
 from django.urls import reverse
 from django.contrib.sessions.models import Session
-from django.test import TestCase
-
-from task_manager.users.models import User
 
 
-class TestAuth(TestCase):
-
-    fixtures = ['tests/functional/fixtures/users.json']
+class TestAuth:
 
     LOGIN_URL = reverse('login')
     LOGOUT_URL = reverse('logout')
 
+    TEST_USER_NAME = 'test_user'
+    TEST_PASS = 'test_pass'
+
     @pytest.fixture
-    def client(self, client):
-        return client
+    def test_user_create(self, django_user_model):
+        return django_user_model.objects.create_user(
+            username=self.TEST_USER_NAME,
+            password=self.TEST_PASS
+        )
 
-
-    def test_login_view(self):
+    def test_login_view(self, client):
         """Тест страницы входа."""
 
-        response = self.client.get(self.LOGIN_URL)
+        response = client.get(self.LOGIN_URL)
         assert response.status_code == 200
         assert response.context['title'] == 'Вход'
         assert '<title>Вход</title>' in response.content.decode('utf8')
         assert '<button class="btn btn-primary" type="submit">Войти</button>' in response.content.decode('utf8')
 
-        users = User.objects.all()
-        assert users.count() == 2
-
-    def test_login(self, user_a, client, create_user_a):
-        """Пользователь залогинился."""
+    def test_login(self, test_user_create, client):
+        """Пользователь может авторизоваться."""
 
         response = client.post(
             self.LOGIN_URL, {
-                'username': user_a['username'],
-                'password': user_a['password1'],
+                'username': self.TEST_USER_NAME,
+                'password': self.TEST_PASS,
             }, follow=True
         )
-        message = list(response.context.get('messages'))[0]
 
         assert response.status_code == 200
         assert response.context['user'].is_active
+
+        message = list(response.context.get('messages'))[0]
         assert 'Вы залогинены' in message.message
         assert Session.objects.count() == 1
 
-    def test_logout(self, client, login_user_a):
-        """Пользователь разлогинился."""
+    def test_logout(self, test_user_create, client):
+        """Пользователь может выйти."""
 
-        response = client.post(reverse('logout'), follow=True)
+        client.login(username=self.TEST_USER_NAME, password=self.TEST_PASS)
+
+        response = client.post(self.LOGOUT_URL, follow=True)
         message = list(response.context.get('messages'))[0]
 
         assert response.status_code == 200
