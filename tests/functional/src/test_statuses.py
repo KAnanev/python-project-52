e11a5a223ad11f8f4b594b7b73f47ffd361c2345
KAnanev <russrage@yandex.ru>
2023-11-04 16:38:00 +0300

    Тесты удаления статуса

--- tests/functional/src/test_statuses.py
+++ tests/functional/src/test_statuses.py
@@ -14,6 +14,7 @@ class TestStatusesView(TestStatusesMixin, TestUserMixin):
                               reverse('delete_status', kwargs={'pk': 1})])
     def test_statuses_view_without_login(self, url, client):
         """Неавторизованный пользователь перенаправляется в login."""
+
         response = client.get(url)
         assert response.status_code == 302
         assert response.url == reverse('login')
@@ -30,7 +31,8 @@ class TestStatusesView(TestStatusesMixin, TestUserMixin):
          (reverse('delete_status', kwargs={'pk': 1}), 'Удаление статуса'),
          ])
     def test_statuses_view_with_login(self, url, title, client, create_test_status, login_test_user):
-        """Авторизованный пользователь видит страницы"""
+        """Авторизованный пользователь видит страницы."""
+
         response = client.get(url, follow=True)
         assert response.status_code == 200
         assert response.context['title'] == title
@@ -40,6 +42,7 @@ class TestStatusesView(TestStatusesMixin, TestUserMixin):
                               reverse('delete_status', kwargs={'pk': 1})])
     def test_statuses_view_login_without_task(self, url, client, login_test_user):
         """Не найдено, если нет статусов."""
+
         response = client.get(url, follow=True)
         assert response.status_code == 404
 
@@ -84,29 +87,29 @@ class TestStatusCRUD(TestUserMixin, TestStatusesMixin):
 
         assert response.status_code == 200
         assert 'Статус успешно изменен' in message.message
-#
-#
-# class TestDeleteStatus(BaseTest):
-#     view_name = 'delete_status'
-#
-#     def test_get_delete_status(self, create_status, login_user_a, client_get):
-#         """Тест страницы удаления статуса."""
-#
-#         response = client_get(pk=create_status.pk)
-#
-#         assert response.status_code == 200
-#         assert response.context['title'] == 'Удаление статуса'
-#         assert 'Да, удалить' in response.content.decode('utf8')
-#         assert f'Вы уверены, что хотите удалить {create_status.name}?' in response.content.decode('utf8')
-#         assert 'class="btn btn-danger"' in response.content.decode('utf8')
-#
-#     def test_post_delete_status(self, create_status, login_user_a, client_post):
-#         """Тест удаления статуса."""
-#
-#         response = client_post(pk=create_status.pk, follow=True)
-#         statuses = TaskStatus.objects.all()
-#         message = list(response.context.get('messages'))[0]
-#
-#         assert response.status_code == 200
-#         assert not len(statuses)
-#         assert 'Статус успешно удален' in message.message
+
+    def test_get_delete_status(self, client, login_test_user, create_test_status):
+        """Тест страницы удаления статуса."""
+
+        status = TaskStatus.objects.first()
+        url = reverse('delete_status', kwargs={'pk': status.pk})
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert response.context['title'] == 'Удаление статуса'
+        assert 'Да, удалить' in response.content.decode('utf8')
+        assert f'Вы уверены, что хотите удалить {status.name}?' in response.content.decode('utf8')
+        assert 'class="btn btn-danger"' in response.content.decode('utf8')
+
+    def test_delete_status(self, client, login_test_user, create_test_status):
+        """Тест удаления статуса."""
+
+        status = TaskStatus.objects.first()
+        url = reverse('delete_status', kwargs={'pk': status.pk})
+        response = client.post(url, follow=True)
+        statuses = TaskStatus.objects.all()
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert not len(statuses)
+        assert 'Статус успешно удален' in message.message
