11f0fc0f92d704f14525f02388eb582283363fc8
KAnanev <russrage@yandex.ru>
2023-11-11 12:45:00 +0300

    Тест создания задачи

--- tests/functional/src/test_tasks.py
+++ tests/functional/src/test_tasks.py
@@ -1,7 +1,8 @@
 import pytest
 from django.urls import reverse
 
-from tests.functional.conftest import TestTasksMixin
+from task_manager.tasks.models import Task
+from tests.functional.conftest import TestTasksMixin, TestUserMixin, TestStatusesMixin
 
 
 class TestTasksView(TestTasksMixin):
@@ -46,33 +47,74 @@ class TestTasksView(TestTasksMixin):
         response = client.get(url, follow=True)
         assert response.status_code == 404
 
-# class TestCreateTask(BaseTask):
-#     url = reverse('create_task')
-#     title = 'Создать задачу'
-#
-#     def test_create_task_without_login(self, response):
-#         super().test_view_tasks_without_login(response)
-#
-#     def test_create_task_with_login(self, login_user_a, response):
-#         super().test_view_tasks_with_login(login_user_a, response)
-#
-#     def test_post_create(self, login_user_a, status_in_db, post_response):
-#         status = TaskStatus.objects.first()
-#         user = User.objects.first()
-#         response = post_response(
-#             self.url, {
-#                 'name': 'Задача',
-#                 'status': status.pk,
-#                 'executor': user.pk,
-#             }
-#             , follow=True
-#         )
-#
-#         assert response.status_code == 200
-#
-#         task = Task.objects.first()
-#         assert task.status == status
-#         assert task.executor == user
-#
-#         message = list(response.context.get('messages'))[0]
-#         assert 'Задача успешно создана' in message.message
+
+class TestStatusCRUD(TestTasksMixin):
+
+    def test_create_task(self, client, login_test_user, create_test_status, create_test_user):
+        """Статус создается."""
+
+        new_name = 'new_name'
+        url = reverse('create_task')
+
+        response = client.post(url, data={
+            'name': new_name,
+            'author': create_test_user.pk,
+            'status': create_test_status.pk,
+        })
+        status = Task.objects.first()
+
+        assert response.status_code == 302
+        assert response.url == reverse('tasks')
+        assert status.name == new_name
+
+        response = client.post(url, data={'name': new_name}, follow=True)
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert 'Задача успешно создана' in message.message
+
+    def test_update_status(self, client, login_test_user, create_test_status):
+        """Тест обновления статуса."""
+
+        new_name = 'new_name'
+        status = TaskStatus.objects.first()
+        url = reverse('update_status', kwargs={'pk': status.pk})
+
+        response = client.post(url, data={'name': new_name})
+        status = TaskStatus.objects.first()
+
+        assert response.status_code == 302
+        assert response.url == reverse('statuses')
+        assert status.name == new_name
+
+        response = client.post(url, data={'name': new_name}, follow=True)
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert 'Статус успешно изменен' in message.message
+
+    def test_get_delete_status(self, client, login_test_user, create_test_status):
+        """Тест страницы удаления статуса."""
+
+        status = TaskStatus.objects.first()
+        url = reverse('delete_status', kwargs={'pk': status.pk})
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert response.context['title'] == 'Удаление статуса'
+        assert 'Да, удалить' in response.content.decode('utf8')
+        assert f'Вы уверены, что хотите удалить {status.name}?' in response.content.decode('utf8')
+        assert 'class="btn btn-danger"' in response.content.decode('utf8')
+
+    def test_delete_status(self, client, login_test_user, create_test_status):
+        """Тест удаления статуса."""
+
+        status = TaskStatus.objects.first()
+        url = reverse('delete_status', kwargs={'pk': status.pk})
+        response = client.post(url, follow=True)
+        statuses = TaskStatus.objects.all()
+        message = list(response.context.get('messages'))[0]
+
+        assert response.status_code == 200
+        assert not len(statuses)
+        assert 'Статус успешно удален' in message.message
